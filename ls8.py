f83f088db61fc6e11c3baa76564e97e7d2dbacd7
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-02-28 10:42:02 -0600

    Progress:
    Copied and pasted from the other assignment to get started and then converted CMP, JMP, JEQ, and JNE. I also just added the less, greater, and equal properties.

---
+++ ls8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+if len(sys.argv) != 2:
+    print('Usage: file.py filename', file=sys.stderr)
+    sys.exit(1)
+cpu.load()
+cpu.run()
